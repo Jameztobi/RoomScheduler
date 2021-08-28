eea38ab96b333f9dbbe74af98b2688adecbd9277
Jameztobi <oluwatobi.james@yahoo.com>
2021-08-29 00:12:49 +0100

    App-setup- security update

--- Application /static/app-setup.js
+++ Application /static/app-setup.js
@@ -2,14 +2,11 @@
  // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyB8xqfry28ukQtSdlDT67K-P-fRXnhbeig",
-  authDomain: "project-test-6cbb5.firebaseapp.com",
-  projectId: "project-test-6cbb5",
-  storageBucket: "project-test-6cbb5.appspot.com",
-  messagingSenderId: "222887722240",
-  appId: "1:222887722240:web:9eaa676286925ca2ce47e5",
-  measurementId: "G-QN48Z5SLY5"
-};
-// Initialize Firebase
+	apiKey: "<API_KEY>",
+	authDomain: "<PROJECT_ID>.firebaseapp.com", 
+	databaseURL: "https://<DATABASE_NAME>.firebaseio.com", 
+	projectId: "<PROJECT_ID>",
+	storageBucket: "<BUCKET>.appspot.com", 
+	messagingSenderId: "<SENDER_ID>",
+}; 
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
